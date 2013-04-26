280da734611f52177fb2166ff6900ec77be48bdb
John Bandela <jbandela@gmail.com>
2013-04-25 21:31:40 -0400

    Latest

--- asio_helper.hpp
+++ asio_helper.hpp
@@ -73,52 +73,69 @@ namespace asio_helper{
 
         };
         // Get return type of function
-        // Adapted from http://stackoverflow.com/questions/13998227/determining-number-and-type-of-parameters-and-return-type-of-type-parameter-that
-        namespace detail {
+        // From http://stackoverflow.com/questions/13998227/determining-number-and-type-of-parameters-and-return-type-of-type-parameter-that
+ namespace detail {
+    ////////////////////////////////////////////////////////////////////////////
+    //! Select between function pointer types
+    ////////////////////////////////////////////////////////////////////////////
+    template <typename T>
+    struct callable_helper_ptr;
 
-            template <typename T>
-            struct return_type_helper{};
+    //! non-member functions
+    template <typename R, typename... Args>
+    struct callable_helper_ptr<R (*)(Args...)> {
+        typedef void                object_t;
+        typedef R                   result_t;
+        typedef std::tuple<Args...> args_t;
+    };
 
-            // non-member functions
-            template <typename R, typename... Args>
-            struct return_type_helper<R (*)(Args...)> {
-                typedef R                   result_t;
-            };
+    //! member functions
+    template <typename R, typename O, typename... Args>
+    struct callable_helper_ptr<R (O::*)(Args...)> {
+        typedef O                   object_t;
+        typedef R                   result_t;
+        typedef std::tuple<Args...> args_t;
+    };
 
-            // member functions
-            template <typename R, typename O, typename... Args>
-            struct return_type_helper<R (O::*)(Args...)> {
-                typedef R                   result_t;
-            };
+    //! const member functions
+    template <typename R, typename O, typename... Args>
+    struct callable_helper_ptr<R (O::*)(Args...) const> {
+        typedef O                   object_t;
+        typedef R                   result_t;
+        typedef std::tuple<Args...> args_t;
+    };
 
-            // const member functions
-            template <typename R, typename O, typename... Args>
-            struct return_type_helper<R (O::*)(Args...) const> {
-                typedef R                   result_t;
-            };
+    ////////////////////////////////////////////////////////////////////////////
+    //! Select between function pointers and functors
+    ////////////////////////////////////////////////////////////////////////////
+    template <typename T, typename is_ptr = typename std::is_pointer<T>::type>
+    struct callable_helper;
 
+    //! specialization for functors (and lambdas)
+    template <typename T>
+    struct callable_helper<T, std::false_type> {
+        typedef callable_helper_ptr<decltype(&T::operator())> type;
+    };
 
-            template <typename T, typename is_ptr = typename std::is_pointer<T>::type>
-            struct callable_helper;
+    //! specialization for function pointers
+    template <typename T>
+    struct callable_helper<T, std::true_type> {
+        typedef callable_helper_ptr<T> type;
+    };
+} //namespace detail
 
-            //! specialization for functors (and lambdas)
-            template <typename T>
-            struct callable_helper<T, std::false_type> {
-                typedef return_type_helper<decltype(&T::operator())> type;
-            };
+////////////////////////////////////////////////////////////////////////////////
+//! defines the various details of a callable object T
+////////////////////////////////////////////////////////////////////////////////
+template <typename T>
+struct callable_traits {
+    typedef typename detail::callable_helper<T>::type::object_t object_t;
+    typedef typename detail::callable_helper<T>::type::result_t result_t;
+    typedef typename detail::callable_helper<T>::type::args_t   args_t;
 
-            //! specialization for function pointers
-            template <typename T>
-            struct callable_helper<T, std::true_type> {
-                typedef return_type_helper<T> type;
-            };
-        } 
-
-        template <typename T>
-        struct callable_traits {
-            typedef typename detail::callable_helper<T>::type::result_t result_t;
-        };
-
+    template <unsigned N>
+    struct arg : public std::tuple_element<N, args_t> {};
+};
     }
 
     namespace handlers{
